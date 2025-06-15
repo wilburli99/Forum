46866fd9446d2cf4923b44285093089826089459
Wilbur <lwb980210@gmail.com>
2025-06-15 13:23:46 +1000

    更新mapper的扩展查询 按照用户名查询用户信息 因为如果直接在UserMapper中添加，可能会导致mybatis generator的重新覆盖，所以要创建一个extension文件夹，把自定义的SQL查询写在里面

--- src/main/java/cn/iocoder/forum/dao/UserMapper.java
+++ src/main/java/cn/iocoder/forum/dao/UserMapper.java
@@ -2,6 +2,7 @@ package cn.iocoder.forum.dao;
 
 import cn.iocoder.forum.model.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
@@ -14,4 +15,6 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User row);
 
     int updateByPrimaryKey(User row);
+
+    User selectByUserName(@Param("username") String username);
 }
